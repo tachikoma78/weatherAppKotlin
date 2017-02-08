cf9757651c7079a2dd2448fcc60a8a489c82e838
Pierre vanderpol <pierre.vanderpol@screeninteraction.com>
2017-02-08 14:30:04 +0100

    added folder

---
+++ weatherApp/app/src/main/java/com/pierre/vanderpol/weatherapp/UI/adapters/ForecastListAdapter.kt
@@ -0,0 +1,23 @@
+package com.pierre.vanderpol.weatherapp.UI.adapters
+
+import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup
+import android.widget.TextView
+
+/**
+ * Created by piva on 2017-02-07.
+ */
+class ForecastListAdapter(val items: List<String>) : RecyclerView.Adapter<ForecastListAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(TextView(parent.context))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.textView.text = items[position]
+    }
+
+    override fun getItemCount(): Int = items.size
+
+    class ViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
+}
---
+++ weatherApp/app/src/main/java/com/pierre/vanderpol/weatherapp/data/Request.kt
@@ -0,0 +1,5 @@
+package com.pierre.vanderpol.weatherapp.data
+
+/**
+ * Created by piva on 2017-02-08.
+ */
